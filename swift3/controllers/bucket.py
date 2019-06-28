murlock/swift3

commit 20f6c1b99ed2cb33848ea659a2955d527a370d89

Allow 'subdir' in versioned_response
This fix is needed to accept Prefix that are only visible with
list-object-versions (as the object is a DeleteMarker)

swift3/controllers/bucket.py
@@ -146,9 +146,11 @@ class BucketController(Controller):
                 resp = req.get_response(self.app, query=query)
                 versioned_objects = json.loads(resp.body)
                 for o in versioned_objects:
-                    # The name looks like this:
-                    #  '%03x%s/%s' % (len(name), name, version)
-                    o['name'], o['version_id'] = o['name'][3:].rsplit('/', 1)
+                    if 'name' in o:
+                        # The name looks like this:
+                        #  '%03x%s/%s' % (len(name), name, version)
+                        o['name'], o['version_id'] = \
+                            o['name'][3:].rsplit('/', 1)
                 objects.extend(versioned_objects)
             except NoSuchBucket:
                 # the bucket may not be versioned

commit c2fe036c25bddd07c6ff725d22096b875a3fa96c

Merge prefixes with versioning
An issue was introduced by 20f6c1b to support shadowed prefixes:
the listing with delimiter could report duplicate prefixes if
same prefix was reported by normal listing and by versioned listing.

swift3/controllers/bucket.py
@@ -145,12 +145,19 @@ class BucketController(Controller):
             try:
                 resp = req.get_response(self.app, query=query)
                 versioned_objects = json.loads(resp.body)
+                prefixes = set()
                 for o in versioned_objects:
                     if 'name' in o:
                         # The name looks like this:
                         #  '%03x%s/%s' % (len(name), name, version)
                         o['name'], o['version_id'] = \
                             o['name'][3:].rsplit('/', 1)
+                    else:
+                        prefixes.add(o['subdir'])
+                # suppress duplicated prefixes
+                for o in list(objects):
+                    if 'subdir' in o and o['subdir'] in prefixes:
+                        objects.remove(o)
                 objects.extend(versioned_objects)
             except NoSuchBucket:
                 # the bucket may not be versioned